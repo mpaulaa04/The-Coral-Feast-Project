6910173c11e293396c7deedfde103e76395bb3d2
mariapa04 <mariapaulaobandosolis@gmail.com>
2025-11-30 15:38:53 -0600

    Se agrega los JS padre de lobby

---
+++ js/missionsDisplay.js
@@ -0,0 +1,51 @@
+app.component('missions-display', {
+  props: {
+    missions: { type: Array, required: true },
+    missionMoney: { type: Number, required: true }
+  },
+
+
+  methods: {
+    exitMissions() {
+      this.$emit('close-modal');
+    },
+
+    claimReward(index) {
+      this.$emit('claim-reward', index);
+    },
+  },
+
+  template: /*html*/ `
+ <div class="missions-bod">
+  <div class="missions-content">
+    
+    <div class="missions-title">
+      <h1 :style="{ color: 'var(--clr-white)', textAlign: 'center' }">MISSIONS</h1>
+    </div>
+
+    <div class="missions-container">
+      <template v-if="missions.length">
+        <mission-card
+          v-for="(mission, index) in missions"
+          :key="index"
+          :mission="mission"
+          :index="index"
+          @claim-reward="claimReward"
+        ></mission-card>
+      </template>
+
+      <div
+        v-else
+        class="missions-empty"
+        :style="{ color: 'var(--clr-white)', fontFamily: 'LexendDeca', textAlign: 'center' }"
+      >
+        No hay misiones activas por ahora.
+      </div>
+    </div>
+    
+  </div>
+  
+  <button class="ok-button" @click="exitMissions">OK!</button>
+</div>
+`,
+});
